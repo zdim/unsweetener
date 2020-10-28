95db4f506d567705ccdaa411fab0c29a1090d4f1
zdim <zdim227@gmail.com>
2020-10-28 16:45:15 -0600

    fixing search query

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,18 +3,17 @@ const controller = require('../controllers/search_results');
 const usda = require('../utils/usda_requests');
 const router = express.Router();
 
-router.route('/search')
-    .post(async (req, res) => {
-        try {
-            const { search } = req.body;
-            if(search) {
-                const results = await usda.search(search);
-                console.log(results);
-                res.send(controller.handleSearchResults(results));
-            }
-        } catch (e) {
-            console.error(e)
-        }
-    });
+router.route('/search').post(async (req, res) => {
+	try {
+		const { search } = req.body;
+		if (search) {
+			const results = await usda.search(search);
+			console.log(results);
+			res.send(controller.handleSearchResults(results));
+		}
+	} catch (e) {
+		console.error(`SEARCH ERROR: ${e}`);
+	}
+});
 
 module.exports = router;
--- server/utils/usda_requests.js
+++ server/utils/usda_requests.js
@@ -1,27 +1,43 @@
 const fetch = require('node-fetch').default;
-const fauna = require('faunadb'),
-	q = fauna.query;
+const faunadb = require('faunadb'),
+	q = faunadb.query;
 //const KEY = process.env.KEY;
-const KEY = process.env.FAUNA_KEY;
-const client = new fauna.client({ secret: KEY });
+const secret = process.env.FAUNA_KEY;
+const client = new faunadb.Client({ secret });
 const searchRequest = async (query) => {
 	//const apiUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${KEY}`;
-	db.query();
+	console.log('SEARCHING FOR -> ' + query);
+	const response = await client.query(
+		q.Filter(
+			q.Paginate(q.Match(q.Index('by_name'), 'description')),
+			q.Lambda(
+				['description', 'brand_owner'],
+				q.ContainsStr(q.LowerCase(q.Var('description')), query)
+			)
+		)
+	);
 
-	const data = {
-		query,
-		dataType: [`Branded`],
-		pageSize: 25,
-		sortOrder: `asc`,
-	};
+	// const res = await client.query(
+	// 	q.Get(q.Ref(q.Collection('items'), '280326402894463496'))
+	// );
 
-	const request = await fetch(apiUrl, {
-		method: 'POST',
-		body: JSON.stringify(data),
-		headers: { 'Content-Type': 'application/json' },
-	});
+	// console.log('RES ' + res);
+	// return res;
+	// const data = {
+	// 	query,
+	// 	dataType: [`Branded`],
+	// 	pageSize: 25,
+	// 	sortOrder: `asc`,
+	// };
 
-	return request.json();
+	// const request = await fetch(apiUrl, {
+	// 	method: 'POST',
+	// 	body: JSON.stringify(data),
+	// 	headers: { 'Content-Type': 'application/json' },
+	// });
+	console.log(`SEARCH RESPONSE: ${JSON.stringify(response)}`);
+	return response;
+	// return request.json();
 };
 
 const itemRequest = async (id) => {
