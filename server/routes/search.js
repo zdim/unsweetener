84abcfe4a41ddd63b4316a5562b27e253d434800
Zach <zdim227@gmail.com>
2020-10-29 00:16:59 -0600

    making new file for fauna calls, just in case

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const controller = require('../controllers/search_results');
 const usda = require('../utils/usda_requests');
+const fauna = require('../utils/faunadb_requests');
 const router = express.Router();
 
 router.route('/search').post(async (req, res) => {
 	try {
 		const { search } = req.body;
 		if (search) {
-			const results = await usda.search(search);
+			const results = await fauna.search(search);
+			//const results = await usda.search(search);
 			console.log(results);
 			res.send(controller.handleSearchResults(results));
 		}
--- server/utils/usda_requests.js
+++ server/utils/usda_requests.js
@@ -1,43 +1,24 @@
 const fetch = require('node-fetch').default;
-const faunadb = require('faunadb'),
-	q = faunadb.query;
-//const KEY = process.env.KEY;
-const secret = process.env.FAUNA_KEY;
-const client = new faunadb.Client({ secret });
+const KEY = process.env.KEY;
+
 const searchRequest = async (query) => {
-	//const apiUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${KEY}`;
+	const apiUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${KEY}`;
 	console.log('SEARCHING FOR -> ' + query);
-	const response = await client.query(
-		q.Filter(
-			q.Paginate(q.Match(q.Index('by_name'), 'description')),
-			q.Lambda(
-				['description', 'brand_owner'],
-				q.ContainsStr(q.LowerCase(q.Var('description')), query)
-			)
-		)
-	);
 
-	// const res = await client.query(
-	// 	q.Get(q.Ref(q.Collection('items'), '280326402894463496'))
-	// );
+	const data = {
+		query,
+		dataType: [`Branded`],
+		pageSize: 25,
+		sortOrder: `asc`,
+	};
 
-	// console.log('RES ' + res);
-	// return res;
-	// const data = {
-	// 	query,
-	// 	dataType: [`Branded`],
-	// 	pageSize: 25,
-	// 	sortOrder: `asc`,
-	// };
+	const request = await fetch(apiUrl, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: { 'Content-Type': 'application/json' },
+	});
 
-	// const request = await fetch(apiUrl, {
-	// 	method: 'POST',
-	// 	body: JSON.stringify(data),
-	// 	headers: { 'Content-Type': 'application/json' },
-	// });
-	console.log(`SEARCH RESPONSE: ${JSON.stringify(response)}`);
-	return response;
-	// return request.json();
+	return request.json();
 };
 
 const itemRequest = async (id) => {
